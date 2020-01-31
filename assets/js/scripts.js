5f63dded874f162a6449c4c146468c142be95e42
fran2488 <frances.deboo@gmail.com>
2020-01-31 15:23:31 +0000

    javascript function created

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,19 @@
-let colorPick = [“red”, “green”, “yellow”, “blue”];
 
+let colorPool = ["red", "green", "yellow", "blue"];
 let i;
+let colorComp= [];
 
-colorComp[colorComp.length] = colorPick[Math.floor(Math.random() * (colorPick.length)];
+function myFunction() {
+newColor = colorPool[Math.floor(Math.random() * (colorPool.length))];
+colorComp[colorComp.length] = newColor;
 
-console.log(colorComp)
+
+$(newColor).style.display = "block";
+}
+;
+
+/* if(userInput === colorComp) {
+    myFunction();
+} else {
+    alert("Game Over!");
+    */
