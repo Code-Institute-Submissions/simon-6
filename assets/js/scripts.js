5e34d308bd12beb395e8c68cfd1bfcc0ff9692c8
fran2488 <frances.deboo@gmail.com>
2020-02-26 19:59:24 +0000

    integrity and cross origin removed from bootstrap link

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,3 @@
-
 let colorComp = [],
     points = 0,
     userArray = [],
@@ -14,8 +13,8 @@ function playGame() {
     const colorPool = ["red", "green", "yellow", "blue"];
     let i = 0;
 
-    colorComp[colorComp.length] = colorPool[Math.floor(Math.random() *
-        (colorPool.length))];
+    colorComp[colorComp.length] =
+        colorPool[Math.floor(Math.random() * colorPool.length)];
 
     /**
      * setInterval() iterates through the colorComp array. 
@@ -25,99 +24,112 @@ function playGame() {
      * One id is used to target a sound clip. The other is used to target the relevant div element in the document. 
      The class is then used to apply a set of CSS styles to the div until the SetTimeout function ends and the class is removed.
      */
-    let timer = setInterval(function () {
+    let timer = setInterval(function() {
         if (i === colorComp.length) {
-            $(".color_btn").children().removeClass("unclickable");
+            $(".color_btn")
+                .children()
+                .removeClass("unclickable");
             clearInterval(timer);
         }
 
         let colorId = "#" + colorComp[i],
             colorClass = colorComp[i],
-
             soundId = colorId + "-beep",
             sound = new Audio();
 
-            sound.src = $(soundId).children().attr("src");
+        sound.src = $(soundId)
+            .children()
+            .attr("src");
 
-        
         $(colorId).addClass(colorClass);
-        $(colorId).parent().addClass("layer");
+        $(colorId)
+            .parent()
+            .addClass("layer");
         $(sound)[0].play();
-        setTimeout(function () {
+        setTimeout(function() {
             $(colorId).removeClass(colorClass);
-            $(colorId).parent().removeClass("layer");
+            $(colorId)
+                .parent()
+                .removeClass("layer");
         }, 800);
 
         i++;
     }, 1000);
-
 } //close of PlayGame function.
-
 
 /********************** USER INPUT **********************/
 
 let i = 0; //i must be declared outside the function so that i can increment everytime a user clicks to add to the array
 
-$(".color_btn").children().click(function () {
-    let colorClass = this.id;
-    let colorId = "#" + this.id;
-    let soundId = colorId + "-beep";
-    userArray[userArray.length] = colorClass;
-    //console.log(colorId + "/." + colorClass + "/" + soundId);
-    console.log(userArray + " vs " + colorComp);
+$(".color_btn")
+    .children()
+    .click(function() {
+        let colorClass = this.id;
+        let colorId = "#" + this.id;
+        let soundId = colorId + "-beep";
+        userArray[userArray.length] = colorClass;
+        //console.log(colorId + "/." + colorClass + "/" + soundId);
+        console.log(userArray + " vs " + colorComp);
 
-    $(colorId).addClass(colorClass + " unclickable");
-    $(colorId).parent().addClass("layer");
-    $(soundId)[0].play();
+        $(colorId).addClass(colorClass + " unclickable");
+        $(colorId)
+            .parent()
+            .addClass("layer");
+        $(soundId)[0].play();
 
-    setTimeout(() => {
-        $(colorId).removeClass(colorClass + " unclickable");
-        $(colorId).parent().removeClass("layer");
-    }, 400);
+        setTimeout(() => {
+            $(colorId).removeClass(colorClass + " unclickable");
+            $(colorId)
+                .parent()
+                .removeClass("layer");
+        }, 400);
 
-    setTimeout(() => {
-        compareIndex();
-    }, 500);
+        setTimeout(() => {
+            compareIndex();
+        }, 500);
 
+        /**
+         * compareIndex() is called every time a colour is clicked on.
+         * compareIndex() compares each index in the accumulating userArray, against the corresponding index in the colorComp array.
+         */
+        function compareIndex() {
+            if (userArray[i] === colorComp[i]) {
+                if (userArray.length === colorComp.length) {
+                    // if the arrays have matching values and length, clear the user array ready for the next round
+                    //console.log(userArray[i] + " vs " + colorComp[i]);
+                    userArray = [];
+                    i = 0;
+                    points += 5;
+                    $("#score").html("points: " + points);
 
-    /**
-    * compareIndex() is called every time a colour is clicked on. 
-    * compareIndex() compares each index in the accumulating userArray, against the corresponding index in the colorComp array.
-     */
-    function compareIndex() {
-        if (userArray[i] === colorComp[i]) {
-            if (userArray.length === colorComp.length) {
-                // if the arrays have matching values and length, clear the user array ready for the next round    
-                //console.log(userArray[i] + " vs " + colorComp[i]);
-                userArray = [];
-                i = 0;
-                points += 5;
-                $("#score").html("points: " + points);
+                    setTimeout(() => {
+                        $(".color_btn")
+                            .children()
+                            .addClass("unclickable");
+                        levelup(); //lines 1-147
+                        playGame(); //lines 12-54
+                    }, 1000);
+                }
 
-                setTimeout(() => {
-                    $(".color_btn").children().addClass("unclickable");
-                    levelup(); //lines 1-147
-                    playGame(); //lines 12-54
-                }, 1000);
+                //if the values match but the arrays are not the same length, allows the user to add the next colour.
+                else {
+                    i++;
+                }
             }
 
-            //if the values match but the arrays are not the same length, allows the user to add the next colour.
+            //if two values don't match, game over.
             else {
-                i++;
+                $("#gamescore").html(points);
+                $("#gameOver").modal();
+                addScore(points);
+                $("#gameOver")
+                    .on("hide.bs.modal", function() {
+                        location.reload();
+                    })
+                    .modal("show");
             }
         }
-
-        //if two values don't match, game over.
-        else {
-            $("#gamescore").html(points);
-            $('#gameOver').modal();
-            addScore(points);
-            $('#gameOver').on('hide.bs.modal', function () {
-                location.reload();
-            }).modal('show');
-        }
-    }
-});
+    });
 
 /************************ LEVEL UP ************************/
 let delay;
@@ -131,14 +143,10 @@ function levelup() {
     if (points == 20) {
         delay = milliseconds.stageTwo;
         console.log("level up!");
-    }
-
-    else if (points == 40) {
+    } else if (points == 40) {
         delay = milliseconds.stageThree;
         console.log("level up");
-    }
-    
-    else {
+    } else {
         delay = milliseconds.stageOne;
         console.log("level unchanged");
     }
@@ -148,22 +156,24 @@ function levelup() {
 function getScores() {
     highScores_Str = localStorage.getItem("scores"); //Returns a string
     if (highScores_Str === null) {
-            highScores_Str = "0,0,0,0,0";
-        }
+        highScores_Str = "0,0,0,0,0";
+    }
     //Convert the highscores from a string to an array (of numbers):
-    return highScores_Str.split(',').map(function (item) {
+    return highScores_Str.split(",").map(function(item) {
         return parseInt(item);
     });
 }
 
 function addScore(points) {
     highScores_Arr = getScores();
-    
+
     if (points < highScores_Arr[4]) {
-       console.log("good effort");
+        console.log("good effort");
     } else {
         highScores_Arr.splice(4, 1, points);
-        highScores_Arr.sort(function (a, b) { return b - a });
+        highScores_Arr.sort(function(a, b) {
+            return b - a;
+        });
         console.log(highScores_Arr);
         localStorage.setItem("scores", highScores_Arr);
     }
@@ -177,7 +187,6 @@ $("#trigger").click(function() {
     $("#3rd").html(highScores_Arr[2]);
     $("#4th").html(highScores_Arr[3]);
     $("#5th").html(highScores_Arr[4]);
-
 });
 
 //****************** GAME CUSTOMISATION *****************
@@ -185,58 +194,90 @@ $("#gameSound").change(function() {
     let customIndex;
     let soundSetting = $("#gameSound").val();
     const sounds = {
-        red: [["/assets/sounds/red.wav", "audio/wav"], ["assets/sounds/cow.wav", "audio/wav"], ["", ""], ["", ""]],
-        green: [["/assets/sounds/green.wav", "audio/wav"], ["assets/sounds/horse.wav", "audio/wav"], ["", ""], ["", ""]],
-        yellow: [ ["/assets/sounds/yellow.wav", "audio/wav"], ["assets/sounds/goat.wav", "audio/wav"], ["", ""], ["", ""]],
-        blue: [["/assets/sounds/blue.wav", "audio/wav"], ["assets/sounds/pig.wav", "audio/wav"], ["", ""], ["", ""]]
+        red: [
+            ["/assets/sounds/red.wav", "audio/wav"],
+            ["assets/sounds/cow.wav", "audio/wav"],
+            ["", ""],
+            ["", ""]
+        ],
+        green: [
+            ["/assets/sounds/green.wav", "audio/wav"],
+            ["assets/sounds/horse.wav", "audio/wav"],
+            ["", ""],
+            ["", ""]
+        ],
+        yellow: [
+            ["/assets/sounds/yellow.wav", "audio/wav"],
+            ["assets/sounds/goat.wav", "audio/wav"],
+            ["", ""],
+            ["", ""]
+        ],
+        blue: [
+            ["/assets/sounds/blue.wav", "audio/wav"],
+            ["assets/sounds/pig.wav", "audio/wav"],
+            ["", ""],
+            ["", ""]
+        ]
     };
-    
+
     if (soundSetting.includes("default") === true) {
         customIndex = 0;
-    }  
-
-    else if (soundSetting.includes("farm") === true) {
+    } else if (soundSetting.includes("farm") === true) {
         customIndex = 1;
-    }
-
-    else if (soundSetting.includes("flat") === true) {
+    } else if (soundSetting.includes("flat") === true) {
         customIndex = 2;
-    }
-
-    else {
+    } else {
         customIndex = 3;
     }
 
-$("#red-beep").children().attr("src", sounds.red[customIndex][0]);
-$("#red-beep").children().attr("type", sounds.red[customIndex][1]);
-$("#green-beep").children().attr("src", sounds.green[customIndex][0]);
-$("#green-beep").children().attr("type", sounds.green[customIndex][1]);
-$("#yellow-beep").children().attr("src", sounds.yellow[customIndex][0]);
-$("#yellow-beep").children().attr("type", sounds.yellow[customIndex][1]);
-$("#blue-beep").children().attr("src", sounds.blue[customIndex][0]);
-$("#blue-beep").children().attr("type", sounds.blue[customIndex][1]);
+    $("#red-beep")
+        .children()
+        .attr("src", sounds.red[customIndex][0]);
+    $("#red-beep")
+        .children()
+        .attr("type", sounds.red[customIndex][1]);
+    $("#green-beep")
+        .children()
+        .attr("src", sounds.green[customIndex][0]);
+    $("#green-beep")
+        .children()
+        .attr("type", sounds.green[customIndex][1]);
+    $("#yellow-beep")
+        .children()
+        .attr("src", sounds.yellow[customIndex][0]);
+    $("#yellow-beep")
+        .children()
+        .attr("type", sounds.yellow[customIndex][1]);
+    $("#blue-beep")
+        .children()
+        .attr("src", sounds.blue[customIndex][0]);
+    $("#blue-beep")
+        .children()
+        .attr("type", sounds.blue[customIndex][1]);
 
-console.log($("#red-beep").children().attr("src"));
-console.log(customIndex);
-console.log(sounds.red[customIndex][0]);
-console.log("Hello World");
-
+    console.log(
+        $("#red-beep")
+            .children()
+            .attr("src")
+    );
+    console.log(customIndex);
+    console.log(sounds.red[customIndex][0]);
+    console.log("Hello World");
 });
 
 /************************ EMAIL FEEDBACK ***********************/
 function sendMail(feedbackForm) {
-    console.log('working');
-    emailjs.send("gmail", "feedback", {
-        "from_name": feedbackForm.name.value,
-        "from_email": feedbackForm.emailaddress.value,
-        "user_feedback": feedbackForm.feedback.value
-    })
-    
-    .then(
-        function (response) {
-            $('.form-data').val('');
-            $('#feedbackModal').modal('hide');
-        }
-    ); return false;
-}
+    console.log("working");
+    emailjs
+        .send("gmail", "feedback", {
+            from_name: feedbackForm.name.value,
+            from_email: feedbackForm.emailaddress.value,
+            user_feedback: feedbackForm.feedback.value
+        })
 
+        .then(function(response) {
+            $(".form-data").val("");
+            $("#feedbackModal").modal("hide");
+        });
+    return false;
+}
